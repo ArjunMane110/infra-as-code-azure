ffb9b10391e3908ca953ab73b58caa22bcfe5aeb
Arjun Mane <arjunmane110@duck.com>
2025-09-07 22:51:55 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,45 @@
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region for deployment"
+  type        = string
+  default     = "East US"
+}
+
+variable "environment" {
+  description = "Deployment environment (dev/test/prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "admin_username" {
+  description = "Admin username for the VM"
+  type        = string
+  default     = "azureuser"
+}
+
+variable "ssh_public_key" {
+  description = "Path to your SSH public key"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}
+
+variable "vm_size" {
+  description = "VM size"
+  type        = string
+  default     = "Standard_B1s"
+}
+
+variable "host_os" {
+  description = "Operating system of the host (used for scripts/templates)"
+  type        = string
+  default     = "linux"
+}
